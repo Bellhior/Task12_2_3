6d4b12e5fd0b746ec1f171ee5c2c2ed65dc53ccc
Bellhior <cliff_burton85@mail.ru>
2023-06-26 12:04:55 +0400

    Fixing tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,5 +1,5 @@
 import unittest
-from utils import arrs
+from unittest_proj.utils import arrs
 
 
 class TestArrs(unittest.TestCase):
@@ -8,7 +8,6 @@ class TestArrs(unittest.TestCase):
         self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
         self.assertEqual(arrs.get(["shell", 18, True], 0, "test"), "shell")
         self.assertEqual(arrs.get([], -1, "test"), "test")
-
 
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
